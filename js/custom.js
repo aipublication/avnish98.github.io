c29ffdb521065e7e88709bbb6b1df51ed59e2693
avnish <avnish98>
2018-09-23 14:23:01 +0530

    removed pipe around titles

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,3 @@
-/*var i  = 0;
-var txt = 'Python Developer';
-var speed = 120;
-
-window.onload = function typeWriter() {
-    if (i < txt.length) {
-      document.getElementById("intro-typed").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-  }*/
-
 window.onload = new Typed("#intro-typed", {
     strings: ["PYTHON DEVELOPER", "COMPUTER SCIENCE STUDENT", "OPEN SOURCE CONTRIBUTOR"],
     startDelay: 500,
